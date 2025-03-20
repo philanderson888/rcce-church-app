fe9f08818573fe0011d5ccf54f3f8ead09e155d4
philanderson888 <philanderson888@hotmail.com>
2025-03-20 05:17:00 +0000

    add events page

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import Navigation from '../components/Navigation';
 
 function Events() {
@@ -12,8 +11,12 @@ function Events() {
       </header>
       
       <main className="max-w-7xl mx-auto px-4 py-8">
-        <div className="bg-white rounded-xl shadow-sm p-6">
-          <p className="text-gray-600">Events content coming soon...</p>
+        <div className="bg-white rounded-xl shadow-sm overflow-hidden">
+          <iframe 
+            src="https://rcce.churchsuite.com/embed/calendar/events?body_bgcolor=F5F6F7&categories=&event_bgcolor=FFFFFF&event_border=&featured=NULL&filters_bgcolor=FFFFFF&heading_color=444444&icon_color=BBBBBB&layout=grid&merge=signup_to_sequence&num_results=&show_categories=0&show_search=0&show_sites=0&text_color=666666" 
+            className="events-iframe"
+            title="Church Events Calendar"
+          />
         </div>
       </main>
     </div>
